499268d776a45f4e7ead50fcc476c47093d173dc
G-Merich <gmerit2030@gmail.com>
2023-08-17 23:45:01 +0000

    interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
-''' module for log parsing '''
+'''A script that reads stdin line by line and computes metrics'''
 
 
 import sys
-total = 0
+
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
 counter = 0
-sc_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
-           '403': 0, '404': 0, '405': 0, '500': 0}
-
-
-def print_data(total):
-    ''' function to print statistics for input '''
-    print('File size: {}'.format(total))
-    for key, value in sorted(sc_dict.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
-
 
 try:
     for line in sys.stdin:
-        rline = line.split(" ")
-        if len(rline) > 4:
-            code = rline[-2]
-            if code in sc_dict.keys():
-                sc_dict[code] += 1
-            filesize = int(rline[-1])
-            total += filesize
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
             counter += 1
+
         if counter == 10:
             counter = 0
-            print_data(total)
-except Exception as ex:
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
     pass
+
 finally:
-    print_data(total)
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
